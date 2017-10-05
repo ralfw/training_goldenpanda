9be1e06d166218fbcd8bc2bf558d646dd136db94
Ralf Westphal <info@ralfw.de>
2017-10-05 14:34:02 +0200

    dummy from roman conversion; from roman service

---
+++ ralfw/scratchpad/fromRomanConversion/Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace fromRomanConversion
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
+
+
+namespace romanConversion
+{
+    public class FromRomanConverter
+    {
+        public static int[] Convert(IEnumerable<string> romanNumbers) {
+            return romanNumbers.Select(r => r.Length).ToArray();
+        }
+    }
+}
