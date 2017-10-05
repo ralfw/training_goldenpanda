5c96487d847eaa0f12cbd4ff9059918aebab24b2
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-10-05 13:47:54 +0200

    renamedn class

---
+++ mmu_dsa/fromRomanConverter/fromRomanConverter/FromRomanConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fromRomanConverter
+{
+    public class FromRomanConverter
+    {
+        public uint[] ConvertRomanNumbersToRoman(string[] inputRomanNumbers)
+        {
+            return new uint[] {};
+        }
+
+    }
+}
