eff5ef9d5f4631c3aa9bee6760fc656464a31ff1
Lars Guse <lars.guse@wago.com>
2017-10-05 15:27:21 +0200

    Changed fixed path to relational path

--- lgu/FromRomanExport/FromRomanExportAdapterTest/_ExportAdapter/Export.cs
+++ lgu/FromRomanExport/FromRomanExportAdapterTest/_ExportAdapter/Export.cs
@@ -9,7 +9,7 @@ namespace FromRomanExportAdapterTest._ExportAdapter
     [TestFixture]
     public class Export
     {
-        private const string OutputPath = @"c:\temp\fromRoman\output\";
+        private const string OutputPath = @"fromRoman\output\";
 
         [SetUp]
         public void SetUp()
