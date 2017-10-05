2202b393d64266efa3e3b756a40e8e6ac2b3cf4e
Lars Guse <lars.guse@wago.com>
2017-10-05 14:40:52 +0200

    Implemented ExportAdapter

---
+++ lgu/FromRomanExport/FromRomanExportAdapter/ExportAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FromRomanExportAdapter
+{
+    public static class ExportAdapter
+    {
+        public static void Export(int[] numbers, string outputDir)
+        {
+            var filename = GenerateUniqueFilename(outputDir);
+
+            var fileContent = numbers.Select(n => n.ToString()).ToArray();
+
+            File.WriteAllLines(filename, fileContent);
+        }
+
+        private static string GenerateUniqueFilename(string outputDir)
+        {
+            return $"{outputDir}{Guid.NewGuid()}.txt";
+        }
+    }
+}
