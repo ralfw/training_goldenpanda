5c8f9bc7b76bd53afd3abc17354b28cdd6155a7b
Jens Knickmeyer <jens.knickmeyer@wago.com>
2017-10-05 14:33:57 +0200

    SiggiUndJens/RomanNumberImportAdapter: some implementations.

---
+++ SiggiUndJens/RomanNumberImportAdapter/RomanNumberImportAdapter.UnitTest/_ImportAdapter/ReadFileNamesInFolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using RomanNumberImportAdapter.UnitTest._ImportAdapter;
+
+namespace RomanNumberImportAdapter.UnitTest
+{
+    [TestFixture]
+    public class ReadFileNamesInFolder : TesterBase
+    {
+        [Test]
+        public void ShouldReadFileNamesInFolder()
+        {
+            var importAdapter = new ImportAdapter("ReadFileNamesInFolder");
+           
+            var result = importAdapter.ReadFileNamesInFolder();
+
+            result.Length.Should().Be(2);
+            result.Should().Contain(new List<string> { "ReadFileNamesInFolder\\a.txt", "ReadFileNamesInFolder\\b.txt" });
+        }
+    }
+}
--- SiggiUndJens/RomanNumberImportAdapter/RomanNumberImportAdapter/ImportAdapter.cs
+++ SiggiUndJens/RomanNumberImportAdapter/RomanNumberImportAdapter/ImportAdapter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,37 @@ namespace RomanNumberImportAdapter
 {
     public class ImportAdapter
     {
-        public string Directory { get; set; }
+        public string ImportDirectory { get; set; }
 
         public ImportAdapter(string directory)
         {
-            Directory = directory;
+            ImportDirectory = directory;
         }
+
         public string[] ReadFileNamesInFolder()
         {
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            return Directory.GetFiles(ImportDirectory);
+        }
+
+        public string[] ReadNumbersFromFiles(string[] filePaths)
+        {
+            var result = new List<string>();
+
+            foreach (var filePath in filePaths)
+            {
+                result.AddRange(File.ReadAllLines(filePath));
+            }
+
+            return result.ToArray();
+        }
+
+        public Tuple<string[], int> Import()
+        {
+            var filePaths = ReadFileNamesInFolder();
+            var numbers = ReadNumbersFromFiles(filePaths);
+            return new Tuple<string[], int>(numbers, filePaths.Length);
             
         }
     }
