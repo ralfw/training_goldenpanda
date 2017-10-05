360ec416a3cc6fd7f774da8fd01ed2e52e4fe918
Ralf Westphal <info@ralfw.de>
2017-10-05 14:02:24 +0200

    roman conversion import adapter

---
+++ ralfw/scratchpad/romanImportAdapter/ImportAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace romanConversion.adapters
+{
+    public class ImportAdapter
+    {
+        public string[] Import(string importDirectory) {
+            var filenames = Find_input_files(importDirectory);
+            var numbers = Import_numbers(filenames);
+            Clear_input_files(filenames);
+            return numbers;
+        }
+        
+        internal string[] Find_input_files(string importDirectory) {
+            return Directory.GetFiles(importDirectory);
+        }
+
+        internal string[] Import_numbers(IEnumerable<string> filenames)
+        {
+            var numbers = filenames.SelectMany(File.ReadAllLines)
+                .Where(r => !string.IsNullOrWhiteSpace(r));
+            return numbers.ToArray();
+        }
+
+        void Clear_input_files(IEnumerable<string> filenames) {
+            filenames.ToList().ForEach(File.Delete);
+        }
+    }
+}
---
+++ ralfw/scratchpad/romanImportAdapter/Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Security.Policy;
+using NUnit.Framework;
+using romanConversion.adapters;
+
+
+namespace fromRoman.adapters.tests
+{
+    [TestFixture]
+    public class test_ImportAdapter
+    {
+        [SetUp]
+        public void Setup() {
+            Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+        }
+        
+        [Test]
+        public void Find_files()
+        {
+            if (Directory.Exists("test_input")) Directory.Delete("test_input", true);
+            Directory.CreateDirectory("test_input");
+            File.WriteAllText("test_input/a.txt", "x");
+            File.WriteAllText("test_input/b.txt", "y");
+
+            var sut = new ImportAdapter();
+            var result = sut.Find_input_files("test_input");
+            Assert.AreEqual(new[] {"test_input/a.txt", "test_input/b.txt"}, result);
+        }
+
+
+        [Test]
+        public void Import_roman_numbers()
+        {
+            if (Directory.Exists("test_input")) Directory.Delete("test_input", true);
+            Directory.CreateDirectory("test_input");
+            File.WriteAllText("test_input/a.txt", "x");
+            File.WriteAllLines("test_input/b.txt", new[]{"y", "z"});
+            
+            var sut = new ImportAdapter();
+            var result = sut.Import_numbers(new[] {"test_input/a.txt", "test_input/b.txt"});
+            
+            Assert.AreEqual(new[]{"x", "y", "z"}, result);
+        }
+
+        
+        [Test]
+        public void Acceptance()
+        {
+            if (Directory.Exists("test_input")) Directory.Delete("test_input", true);
+            Directory.CreateDirectory("test_input");
+            File.WriteAllText("test_input/a.txt", "x");
+            File.WriteAllLines("test_input/b.txt", new[]{"y", "z"});
+            
+            var sut = new ImportAdapter();
+            var result = sut.Import("test_input");
+            
+            Assert.AreEqual(new[]{"x", "y", "z"}, result);
+            Assert.AreEqual(0, Directory.GetFiles("test_input").Length);
+        }
+    }
+}
