148b9138b47bca31f55e85a2cd06f1f1a9feb783
Jens Knickmeyer <jens.knickmeyer@wago.com>
2017-10-05 13:54:07 +0200

    SiggiUndJens/RomanNumberImportAdapter: initial checkin.

---
+++ SiggiUndJens/RomanNumberImportAdapter/RomanNumberImportAdapter.UnitTest/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace RomanNumberImportAdapter.UnitTest
+{
+    [TestFixture]
+    public class Import
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Directory.CreateDirectory("Import");
+
+        }
+
+        [Test]
+        public void ShouldReadFileNamesInFolder()
+        {
+            var importAdapter = new ImportAdapter("Import");
+
+            var result = importAdapter.ReadFileNamesInFolder();
+
+            result.Length.Should().Be(4);
+        }
+    }
+}
