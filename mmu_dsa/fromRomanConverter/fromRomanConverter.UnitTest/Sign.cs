d057d3729322f990ca9d4bf3f67f663c5dcf3a62
Viktor Nesterow <viktor.nesterow@wago.com>
2017-10-16 09:46:52 +0200

    Sign: Tests completed.

--- mmu_dsa/fromRomanConverter/fromRomanConverter.UnitTest/Sign.cs
+++ mmu_dsa/fromRomanConverter/fromRomanConverter.UnitTest/Sign.cs
@@ -17,12 +17,16 @@ namespace fromRomanConverter.UnitTest
 
         public static IEnumerable<TestCaseData> GetTestCaseData()
         {
+            var testCaseData = new TestCaseData(new[] { 5, 1 }, new[] { 5, 1 });
+            testCaseData.SetName("ShouldNotSignNumbers");
+            yield return testCaseData;
+
             var testCaseData1 = new TestCaseData(new[] { 1, 5 }, new[] { -1, 5 });
-            testCaseData1.SetName("Test1");
+            testCaseData1.SetName("ShouldSignFirstNumber");
             yield return testCaseData1;
 
             var testCaseData2 = new TestCaseData(new[] { 1, 5, 10 }, new[] { -1, -5, 10 });
-            testCaseData2.SetName("Test2");
+            testCaseData2.SetName("ShouldSignFirstTwoNumbers");
             yield return testCaseData2;
         }
     }
