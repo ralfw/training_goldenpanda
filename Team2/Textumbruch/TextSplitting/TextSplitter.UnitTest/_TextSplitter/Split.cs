7552def5f53c045167dd2b1312fe4ee5103ff2bf
Viktor Nesterow <viktor.nesterow@wago.com>
2017-10-20 11:32:49 +0200

    Team2/Textumbruch/TextSplitting: initial check-in, Split method tested.

---
+++ Team2/Textumbruch/TextSplitting/TextSplitter.UnitTest/_TextSplitter/Split.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TextSplitter.UnitTest._TextSplitter
+{
+    [TestFixture]
+    public class Split
+    {
+        [TestCase("Es blaut die Nacht, gagelpu", new[] {"Es", "blaut", "die", "Nacht,", "gagelpu"})]
+        [TestCase("Es\tblaut\ndie\rNacht, gagelpu", new[] { "Es", "blaut", "die", "Nacht,", "gagelpu" })]
+        public void ShouldSplitTextIntoSingleWords(string text, string[] expectedWords)
+        {
+            var textSplitter = new TextSplitter();
+
+            var result = textSplitter.Split(text);
+
+            result.Should().ContainInOrder(expectedWords);
+        }
+    }
+}
---
+++ Team2/Textumbruch/TextSplitting/TextSplitter/TextSplitter.cs
@@ -0,0 +1,10 @@
+﻿namespace TextSplitter
+{
+    public class TextSplitter
+    {
+        public string[] Split(string text)
+        {
+            return text.Split(new char[] {'\n', '\r', '\t', ' '});
+        }
+    }
+}
