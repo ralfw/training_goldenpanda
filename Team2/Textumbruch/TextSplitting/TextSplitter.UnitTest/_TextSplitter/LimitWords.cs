0641f6ba73e20d669fe3f923c03d86e43e091d09
Viktor Nesterow <viktor.nesterow@wago.com>
2017-10-20 11:58:12 +0200

    Team2/Textumbruch/TextSplitting: LimitWords added.

---
+++ Team2/Textumbruch/TextSplitting/TextSplitter.UnitTest/_TextSplitter/LimitWords.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TextSplitter.UnitTest._TextSplitter
+{
+    [TestFixture]
+    public class LimitWords
+    {
+        [Test]
+        public void ShouldLimitWords()
+        {
+            var sut = new TextSplitter();
+            var input = new[] {"die", "Nacht"};
+            var expected = new[] { "die", "Nac","ht" };
+            const int linelength = 3;
+
+            var result = sut.LimitWords(input, linelength);
+
+            result.Should().ContainInOrder(expected);
+        }
+    }
+}
--- Team2/Textumbruch/TextSplitting/TextSplitter/TextSplitter.cs
+++ Team2/Textumbruch/TextSplitting/TextSplitter/TextSplitter.cs
@@ -1,10 +1,38 @@
-﻿namespace TextSplitter
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextSplitter
 {
     public class TextSplitter
     {
         public string[] Split(string text)
         {
             return text.Split(new char[] {'\n', '\r', '\t', ' '});
+        }
+
+        public IEnumerable<string> LimitWords(string[] words, int limit)
+        {
+            var result = new List<string>();
+            foreach (var word in words)
+            {
+                result.AddRange(LimitWord(word,limit));
+            }
+            return result;
+        }
+
+        public IEnumerable<string> LimitWord(string word, int limit)
+        {
+            var result = new List<string>();
+            var currentWord = word;
+            while (currentWord.Length > 0)
+            {
+                var charCount = Math.Min(limit, currentWord.Length);
+                var limitedWord = currentWord.Substring(0, charCount);
+                result.Add(limitedWord);
+                currentWord = currentWord.Substring(charCount, currentWord.Length - charCount);
+            }
+
+            return result;
         }
     }
 }
