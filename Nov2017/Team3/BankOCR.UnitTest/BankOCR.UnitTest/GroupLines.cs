d49bfb6d6274e2524751350b44dedb2e0d7ee8e5
Viktor Nesterow <viktor.nesterow@wago.com>
2017-11-14 17:43:28 +0100

    Function 'GroupLines' implemented.

---
+++ Nov2017/Team3/BankOCR.UnitTest/BankOCR.UnitTest/GroupLines.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BankOCR.UnitTest
+{
+    [TestFixture]
+    public class GroupLines
+    {
+        [Test]
+        public void ShouldCreateAccountNumber()
+        {
+            List<string> lines = new List<string>();
+            lines.Add(" _ _     _ _  _ _  _ _");
+            lines.Add("I _I _II_II_ I_   II_II_II I");
+            lines.Add("I I_  _I I _II_I II_I _II_I");
+            lines.Add("                           ");
+
+            var sut = new SevenSegmentParser();
+            var sevenSegmentNumbers = sut.GroupLines(lines);
+
+            sevenSegmentNumbers.Should().HaveCount(1);
+            var sevenSegmentAccountNumber = sevenSegmentNumbers[0];
+
+            sevenSegmentAccountNumber.Line1.Should().Be(lines[0]);
+            sevenSegmentAccountNumber.Line2.Should().Be(lines[1]);
+            sevenSegmentAccountNumber.Line3.Should().Be(lines[2]);
+
+        }
+        [Test]
+        public void ShouldCreateMultipleAccountNumbers()
+        {
+            List<string> lines = new List<string>();
+            lines.Add(" _ _     _ _  _ _  _ _");
+            lines.Add("I _I _II_II_ I_   II_II_II I");
+            lines.Add("I I_  _I I _II_I II_I _II_I");
+            lines.Add("                           ");
+            lines.Add(" _ _     _ _  _ _  _ _");
+            lines.Add("I _I _II_II_ I_   II_II_II I");
+            lines.Add("I I_  _I I _II_I II_I _II_I");
+            lines.Add("                           ");
+
+            var sut = new SevenSegmentParser();
+            var sevenSegmentNumbers = sut.GroupLines(lines);
+
+            sevenSegmentNumbers.Should().HaveCount(2);
+            var sevenSegmentAccountNumber = sevenSegmentNumbers[0];
+
+            sevenSegmentAccountNumber.Line1.Should().Be(lines[0]);
+            sevenSegmentAccountNumber.Line2.Should().Be(lines[1]);
+            sevenSegmentAccountNumber.Line3.Should().Be(lines[2]);
+
+            var sevenSegmentAccountNumber2 = sevenSegmentNumbers[1];
+
+            sevenSegmentAccountNumber2.Line1.Should().Be(lines[4]);
+            sevenSegmentAccountNumber2.Line2.Should().Be(lines[5]);
+            sevenSegmentAccountNumber2.Line3.Should().Be(lines[6]);
+        }
+    }
+}
---
+++ Nov2017/Team3/BankOCR/BankOCR/SevenSegmentAccountNumber.cs
@@ -0,0 +1,9 @@
+﻿namespace BankOCR
+{
+    public class SevenSegmentAccountNumber
+    {
+        public string Line1 { get; set; }
+        public object Line2 { get; set; }
+        public object Line3 { get; set; }
+    }
+}
