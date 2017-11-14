9fbda1a9a956849b95798c05998a54b47e030a66
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-11-14 16:55:56 +0100

    nexts

---
+++ Nov2017/Team3/BankOCR.UnitTest/BankOCR.UnitTest/SevenSegmentAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BankOCR.UnitTest
+{
+    [TestFixture]
+    public class SevenSegmentAccountTest
+    {
+        [Test]
+        public void ShouldGenerateSingleAccountForDigit4()
+        {
+            SevenSegmentAccount account = new SevenSegmentAccount();
+
+            account.line1 = "   ";
+            account.line2 = "|_|";
+            account.line3 = "  |";
+
+            account.GetDigits().Count().Should().Be(1);
+            string result = account.GetDigits()[0].SevenSegments;
+            result.Should().Be("   |_|  |");
+
+
+        }
+    }
+}
