65ccfb1451d833ae143191ddcf47fec81a5f82c6
Johann Dück <johann.dueck@wago.com>
2017-11-14 15:41:03 +0100

    Initial Commit for Team2

---
+++ Nov2017/Team2/BankOCR.UnitTest/Map.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BankOCR.UnitTest
+{
+    [TestFixture]
+    public class Map
+    {
+        [Test]
+        public void ShouldMapSegmentsToNumber()
+        {
+            var sut = new SevenSegmentDigit("   " +
+                                            "  |" +
+                                            "  |");
+
+            var mappedSignatur = sut.Map();
+            mappedSignatur.Should().Be('1');
+        }
+
+    }
+}
