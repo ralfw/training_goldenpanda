f8e0d40be26418803401faf01d84c2d70627bdd0
Matthias Müther <matthias.muether@wago.com>
2017-11-14 16:16:32 +0100

    Team3: Classes added

---
+++ Nov2017/Team3/BankOCR/BankOCR/SevenSegmentAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOCR
+{
+    public class SevenSegmentAccount
+    {
+        public string line1;
+        public string line2;
+        public string line3;
+
+
+    }
+}
---
+++ Nov2017/Team3/BankOCR/BankOCR/SevenSegmentDigit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOCR
+{
+    public class SevenSegmentDigit
+    {
+        public string SevenSegments { get; set; }
+
+        private char Map()
+        {
+
+        }
+    }
+}
