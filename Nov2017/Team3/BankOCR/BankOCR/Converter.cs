923249ff8acccc58854b78616dd743b5a194927f
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-11-15 11:25:35 +0100

    some rafactorings

--- Nov2017/Team3/BankOCR/BankOCR/Converter.cs
+++ Nov2017/Team3/BankOCR/BankOCR/Converter.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankOCR
 {
@@ -10,21 +8,19 @@ namespace BankOCR
     {
         public static string[] Convert(SevenSegmentAccountNumber[] input)
         {
-            IList<string> resultsList = new List<String>();
-
-            foreach (var acc in input)
-            {
-                var chars = acc.GetDigits().Select(_ => _.Map()).ToList();
-
-                string account = chars.Aggregate("", (current, ch) => current + ch);
-
-                resultsList.Add(account);
-            }
-
-
+            IList<string> resultsList = input.Select(GetAccount).ToList();
             return resultsList.ToArray();
-
         }
 
+        #region Private methods
+
+        private static String GetAccount(SevenSegmentAccountNumber acc)
+        {
+            var chars = acc.GetDigits().Select(_ => _.Map()).ToList();
+            string account = chars.Aggregate("", (current, ch) => current + ch);
+            return account;
+        }
+
+        #endregion
     }
 }
