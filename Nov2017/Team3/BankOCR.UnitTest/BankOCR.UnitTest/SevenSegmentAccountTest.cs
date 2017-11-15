f879e4fc41a50d72e1c16215b29133edc7a5b471
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-11-15 08:25:25 +0100

    more tests added

--- Nov2017/Team3/BankOCR.UnitTest/BankOCR.UnitTest/SevenSegmentAccountTest.cs
+++ Nov2017/Team3/BankOCR.UnitTest/BankOCR.UnitTest/SevenSegmentAccountTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -9,19 +8,79 @@ namespace BankOCR.UnitTest
     public class SevenSegmentAccountTest
     {
         [Test]
-        public void ShouldGenerateSingleAccountForDigit4()
+        public void ShouldGenerateSingleAccountForDigit1()
         {
-            SevenSegmentAccountNumber account = new SevenSegmentAccountNumber();
+            SevenSegmentAccount account = new SevenSegmentAccount();
 
-            account.Line1 = "   ";
-            account.Line2 = "|_|";
-            account.Line3 = "  |";
+            account.line1 = line1[0];
+            account.line2 = line1[1];
+            account.line3 = line1[2];
 
             account.GetDigits().Count().Should().Be(1);
             string result = account.GetDigits()[0].SevenSegments;
-            result.Should().Be("   |_|  |");
-
-
+            result.Should().Be(segments1);
         }
+
+        [Test]
+        public void ShouldGenerateSingleAccountForDigit2()
+        {
+            SevenSegmentAccount account = new SevenSegmentAccount();
+
+            account.line1 = line2[0];
+            account.line2 = line2[1];
+            account.line3 = line2[2];
+
+            account.GetDigits().Count().Should().Be(1);
+            string result = account.GetDigits()[0].SevenSegments;
+            result.Should().Be(segments2);
+        }
+
+        [Test]
+        public void ShouldGenerateTwoAccountaForDigit12()
+        {
+            SevenSegmentAccount account = new SevenSegmentAccount();
+
+            account.line1 = line1[0] + line2[0];
+            account.line2 = line1[1] + line2[1];
+            account.line3 = line1[2] + line2[2];
+
+            account.GetDigits().Count().Should().Be(2);
+            string result1 = account.GetDigits()[0].SevenSegments;
+            string result2 = account.GetDigits()[1].SevenSegments;
+            result1.Should().Be(segments1);
+            result2.Should().Be(segments2);
+        }
+
+        private readonly string[] line1 =
+        {
+            "   ",
+            "  I",
+            "  I"
+        };
+
+        private readonly string segments1 = "   " +
+                                            "  I" +
+                                            "  I";
+
+        private readonly string[] line2 =
+        {
+            " _ ",
+            " _I",
+            " I_"
+        };
+
+        private readonly string segments2 = " _ " +
+                                            " _I" +
+                                            " I_";
+
+        private string[] line4 = {
+            "   ",
+            "I_I",
+            "  I"
+        };
+
+        private string segments4 = "   " +
+                                   "I_I" +
+                                   "  I";
     }
 }
