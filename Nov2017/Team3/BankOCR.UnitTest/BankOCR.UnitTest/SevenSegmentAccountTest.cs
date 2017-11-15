b24acc400966557cee98f9aa3aa02867ea7a2339
Viktor Nesterow <viktor.nesterow@wago.com>
2017-11-15 08:17:11 +0100

    Fixed merge.

--- Nov2017/Team3/BankOCR.UnitTest/BankOCR.UnitTest/SevenSegmentAccountTest.cs
+++ Nov2017/Team3/BankOCR.UnitTest/BankOCR.UnitTest/SevenSegmentAccountTest.cs
@@ -11,11 +11,11 @@ namespace BankOCR.UnitTest
         [Test]
         public void ShouldGenerateSingleAccountForDigit4()
         {
-            SevenSegmentAccount account = new SevenSegmentAccount();
+            SevenSegmentAccountNumber account = new SevenSegmentAccountNumber();
 
-            account.line1 = "   ";
-            account.line2 = "|_|";
-            account.line3 = "  |";
+            account.Line1 = "   ";
+            account.Line2 = "|_|";
+            account.Line3 = "  |";
 
             account.GetDigits().Count().Should().Be(1);
             string result = account.GetDigits()[0].SevenSegments;
--- Nov2017/Team3/BankOCR/BankOCR/SevenSegmentAccountNumber.cs
+++ Nov2017/Team3/BankOCR/BankOCR/SevenSegmentAccountNumber.cs
@@ -1,9 +1,29 @@
-﻿namespace BankOCR
+﻿using System.Collections.Generic;
+
+namespace BankOCR
 {
     public class SevenSegmentAccountNumber
     {
+        public SevenSegmentDigit[] GetDigits()
+        {
+            List<SevenSegmentDigit> arrResult = new List<SevenSegmentDigit>();
+
+            for (int i = 0; i < Line1.Length; i = i + 3)
+            {
+                string val = Line1[i].ToString() + Line1[i + 1] + Line1[i + 2];
+                val = val + Line2[i] + Line2[i + 1] + Line2[i + 2];
+                val = val + Line3[i] + Line3[i + 1] + Line3[i + 2];
+
+                SevenSegmentDigit digit = new SevenSegmentDigit(val);
+
+                arrResult.Add(digit);
+            }
+
+            return arrResult.ToArray();
+        }
+
         public string Line1 { get; set; }
-        public object Line2 { get; set; }
-        public object Line3 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
     }
 }
