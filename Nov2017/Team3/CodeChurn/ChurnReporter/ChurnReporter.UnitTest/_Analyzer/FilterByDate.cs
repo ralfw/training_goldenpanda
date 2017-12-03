ab88bcde9c2a528c892f35eb720d77b833d35969
nestorvik <v.nesterow@arcor.de>
2017-12-03 22:06:39 +0100

    Team3/ChodeChurn: ChurnReporter: Test for function FilterByDate. WIP.

--- Nov2017/Team3/CodeChurn/ChurnReporter/ChurnReporter.UnitTest/_Analyzer/FilterByDate.cs
+++ Nov2017/Team3/CodeChurn/ChurnReporter/ChurnReporter.UnitTest/_Analyzer/FilterByDate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace ChurnReporter.UnitTest._Analyzer
@@ -9,7 +10,9 @@ namespace ChurnReporter.UnitTest._Analyzer
         [Test]
         public void ShouldFilterEntriesByDate()
         {
-            
+            var startTime = "03.12.2017 17:58:09";
+            var endTime = "03.12.2017 17:19:12";
+            var protocolFilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"./testFiles/protocol.csv");
         }
     }
 }
