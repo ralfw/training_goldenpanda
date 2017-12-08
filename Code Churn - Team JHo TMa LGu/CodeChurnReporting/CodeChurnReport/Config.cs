efb852255b8c235050df99e265e80c3dd8150c4b
Lars Guse <lars.guse@wago.com>
2017-12-08 10:54:25 +0100

    Added ConfigProvider

---
+++ Code Churn - Team JHo TMa LGu/CodeChurnReporting/CodeChurnReport/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChurnReport
+{
+    public struct Config
+    {
+        public string ProtocolFilePath;
+        public DateTime StartDate;
+        public DateTime EndDate;
+    }
+}
---
+++ Code Churn - Team JHo TMa LGu/CodeChurnReporting/CodeChurnReport/ConfigProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChurnReport
+{
+    public static class ConfigProvider
+    {
+        public static Config GetConfig(string[] args)
+        {
+            if (args.Length != 3)
+                throw new ArgumentException();
+            var startDate = DateTime.Parse(args[1], CultureInfo.InvariantCulture);
+            var endDate = DateTime.Parse(args[2], CultureInfo.InvariantCulture);
+            if (endDate < startDate)
+                throw new ArgumentException("StartDate should be less or equal EndDate");
+            return new Config
+            {
+                ProtocolFilePath = args[0],
+                StartDate = startDate,
+                EndDate = endDate
+            };
+        }
+    }
+}
