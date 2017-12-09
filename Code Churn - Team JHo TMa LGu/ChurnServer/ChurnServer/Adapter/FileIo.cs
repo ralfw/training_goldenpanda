874385475a0a5ec7d93ecaa6dd8cf3e76cb623d7
Thomas Mann <someone@someurl.com>
2017-12-09 17:23:41 +0100

    Added Integrationtest project for FileIo adapter
    - implemented GetObservableFiles

---
+++ Code Churn - Team JHo TMa LGu/ChurnServer/ChurnServer/Adapter/FileIo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ChurnServer.AdapterInterfaces;
+
+namespace ChurnServer.Adapter
+{
+    public class FileIo : IFileIo
+    {
+        public IEnumerable<string> GetObservableFiles(string observableDirectoryPath, string[] fileExtensions)
+        {
+            var files = new List<string>();
+            foreach (var fileExtension in fileExtensions.ToArray())
+                files.AddRange(Directory.GetFiles(observableDirectoryPath, $"*.{fileExtension}", SearchOption.AllDirectories));
+
+            return files;
+        }
+    }
+}
