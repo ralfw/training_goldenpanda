ec5713611f122226205cbc8c94a6557a2d9f5d62
Thomas Mann <thomas.mann@wago.com>
2017-12-11 17:01:46 +0100

    Added implementation for Ui

---
+++ Code Churn - Team JHo TMa LGu/ChurnServer/ChurnServer/Adapter/Ui.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using ChurnServer.AdapterInterfaces;
+using ChurnServer.Statistic;
+
+namespace ChurnServer.Adapter
+{
+    public class Ui : IUi
+    {
+        public void ShowStatistic(ReportStatistic statistic)
+        {
+            var lineBuilder = new StringBuilder();
+            lineBuilder.Append(statistic.StartTime);
+            lineBuilder.Append(", ");
+            lineBuilder.AppendFormat("{0} sec", statistic.Duration.TotalSeconds);
+            lineBuilder.Append(", ");
+            lineBuilder.AppendFormat("{0} files", statistic.NumberOfFiles);
+
+            Console.Out.WriteLine(lineBuilder.ToString());
+        }
+    }
+}
