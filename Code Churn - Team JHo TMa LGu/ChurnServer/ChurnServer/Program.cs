343f7f09ae74fcec4d04e511fb90a88a7b2d36af
Thomas Mann <someone@someurl.com>
2017-12-11 21:41:29 +0100

    Updated ChurnServer to execute protocol generation by given time intervals

--- Code Churn - Team JHo TMa LGu/ChurnServer/ChurnServer/Program.cs
+++ Code Churn - Team JHo TMa LGu/ChurnServer/ChurnServer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using ChurnServer.Adapter;
 using ChurnServer.Infrastructure;
 
@@ -6,19 +8,34 @@ namespace ChurnServer
 {
     class Program
     {
+        private static ChurnServerConfiguration Configuration { get; set; }
+
         static void Main(string[] args)
         {
+            Configuration = ChurnServerConfigurationProvider.CreateConfiguration(args);
+
             AdapterProvider.TimeProvider = new TimeProvider();
             AdapterProvider.FileIo = new FileIo();
             AdapterProvider.Ui = new Ui();
+            
+            using (AdapterProvider.TimeProvider.StartTimer(Configuration.SamplingRateInSeconds, GenerateReport))
+            {
+                Console.Out.WriteLine("");
+                Console.Out.WriteLine("Churnserver.exe is observing '{0}' every {1} sec.", Configuration.ObservableDirectoryPath, Configuration.SamplingRateInSeconds);
+                Console.Out.WriteLine("Writing to '{0}'", Configuration.ProtocolFilePath);
+                Console.Out.WriteLine("");
+                Console.Out.WriteLine("Any key to stop the server and exit");
+                Console.Out.WriteLine("");
+                Console.ReadKey();
+            }
+        }
 
-            var configuration = ChurnServerConfigurationProvider.CreateConfiguration(args);
-
-            DumpConfiguration(configuration);
-
-            // todo add timing
-
-            ProtocolBuilder.GenerateReport(configuration.ObservableDirectoryPath, configuration.ProtocolFilePath, configuration.FileExtensions);
+        private static void GenerateReport(object o)
+        {
+            ProtocolBuilder.GenerateReport(
+                Configuration.ObservableDirectoryPath,
+                Configuration.ProtocolFilePath,
+                Configuration.FileExtensions);
         }
 
         private static void DumpConfiguration(ChurnServerConfiguration configuration)
