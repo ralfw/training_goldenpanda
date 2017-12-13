9dcc107571cf7e20ddfd6370479e023c409169fc
Ralf Westphal <info@ralfw.de>
2017-12-13 18:57:39 +0100

    dropstack demo

---
+++ ralfw/servicehostdemo/myservice/MyService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using servicehost.contract;
+
+namespace myservice
+{
+    [Service]
+    public class MyService
+    {
+        [EntryPoint(HttpMethods.Get, "/api/v1/store")] // usage: /api/v1/store?text=hello
+        public string Store(string text)
+        {
+            var dbfilename = Environment.GetEnvironmentVariable("WAGOCREDENTIALS_DBFILENAME");
+            Console.WriteLine($"Store in Db: {dbfilename}");
+            File.WriteAllText(dbfilename, text);          
+            Console.WriteLine("Stored: {0}", text);
+
+            return $"Stored @ {DateTime.Now}";
+        }
+
+
+        [EntryPoint(HttpMethods.Get, "/api/v1/load")] // usage: /api/v1/load
+        public string Load()
+        {
+            var dbfilename = Environment.GetEnvironmentVariable("WAGOCREDENTIALS_DBFILENAME");
+            var text = File.ReadAllText(dbfilename);
+            
+            Console.WriteLine($"Loaded: {text}");
+
+            return $"Loaded '{text.ToUpper()}' @ {DateTime.Now}";
+        }
+    }
+}
