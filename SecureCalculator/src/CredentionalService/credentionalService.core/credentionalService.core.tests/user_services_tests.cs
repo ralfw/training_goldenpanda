c9b651e779aa47043490fb24aaca713e475b05c2
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-12-14 16:28:02 +0100

    add user implemented

---
+++ SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core.tests/user_services_tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace credentionalService.core.tests
+{
+    [TestFixture]
+    internal class user_services_tests
+    {
+        [Test, Ignore]
+        public void ShouldAddUser()
+        {
+            user_services service = new user_services();
+            service.AddUser("nikolai.falke@wago.com", "Student", () => { }, p => { });
+        }
+    }
+}
