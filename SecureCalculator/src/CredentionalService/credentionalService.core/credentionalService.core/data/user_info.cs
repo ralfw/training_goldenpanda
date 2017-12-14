235ac49d062b05d2bddc3a641b499f20ce5da67b
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-12-14 14:47:03 +0100

    db_provider implemented

--- SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core/data/user_info.cs
+++ SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core/data/user_info.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using sc.contracts;
 
 namespace credentionalService.core.data
 {
@@ -10,6 +11,6 @@ namespace credentionalService.core.data
     {
         public string user_email;
         public string password_hash;
-
+        public PermissionSet permissions;
     }
 }
