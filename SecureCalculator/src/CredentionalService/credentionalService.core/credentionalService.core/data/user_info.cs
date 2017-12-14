66878b2a7470274c07827ae1907ba2e96c101459
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-12-14 13:59:31 +0100

    new file added ..

---
+++ SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core/data/user_info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace credentionalService.core.data
+{
+    class user_info
+    {
+        public string user_email;
+        public string password_hash;
+
+    }
+}
