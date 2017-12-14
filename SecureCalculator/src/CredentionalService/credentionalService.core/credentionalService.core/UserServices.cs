0aac677ffcdd8812ada727638be0c28ea7c3dc46
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-12-14 13:31:04 +0100

    hello world !!

---
+++ SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core/UserServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sc.contracts;
+
+namespace credentionalService.core
+{
+    public class UserServices : IUserService
+    {
+        public void AddUser(String emailAddress, String rolename, Action onSuccess, Action<String> onError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogIn(String emailAddress, String passwordHash, Action<PermissionSet> onSuccess, Action<String> onError)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
