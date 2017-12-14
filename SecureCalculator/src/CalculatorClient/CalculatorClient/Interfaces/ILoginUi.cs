f814dff4e7bde0628f372c790c6210c3233a089f
Thomas Mann <thomas.mann@wago.com>
2017-12-14 15:21:32 +0100

    Added ILoginUi interface

---
+++ SecureCalculator/src/CalculatorClient/CalculatorClient/Interfaces/ILoginUi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CalculatorClient.Interfaces
+{
+    public interface ILoginUi
+    {
+        void Open();
+        void Display(string errorMessage);
+        Action<string, string> OnLoginRequested();
+    }
+}
