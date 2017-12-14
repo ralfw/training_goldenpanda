0a4636e1216596d434eeb9148b0e52654cf5439a
Nikolai Falke <nikolai.falke@wago.com>
2017-12-14 15:22:03 +0100

    added mailgun adapter

---
+++ SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core.tests/email_adapter_integration_test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using credentionalService.core.adapter;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace credentionalService.core.tests
+{
+    [TestFixture]
+    public class email_adapter_integration_test
+    {
+        [Test]
+        public void ShouldThrowExceptionIfAddressInvalid()
+        {
+            var mail = new mailgun_adapter();
+
+            Action act = () => { mail.NotifyUser("asdfwagocom", "Welcome new user", "pw"); };
+
+            act.ShouldThrow<Exception>();
+        }
+
+        [Test]
+        public void ShouldSendMail()
+        {
+            var mail = new mailgun_adapter();
+
+            Action act = () => { mail.NotifyUser("nikolai.falke@wago.com", "Welcome new user", "pw"); };
+
+            act.ShouldNotThrow<Exception>();
+        }
+    }
+}
