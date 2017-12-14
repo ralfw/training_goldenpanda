a1a513185e20aa4ca3487d097443ac8af552e02d
Dimitri Samorukov <dimitri.samorukov@wago.com>
2017-12-14 14:47:03 +0100

    db_provider implemented

---
+++ SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core.tests/db_provider_test.cs
@@ -0,0 +1,24 @@
+﻿using credentionalService.core.adapter;
+using FluentAssertions;
+using NUnit.Framework;
+using sc.contracts;
+
+namespace credentionalService.core.tests
+{
+    [TestFixture]
+    public class db_provider_test
+    {
+        [Test, Ignore]
+        public void ShouldInsertUserAndPermissions()
+        {
+            db_provider dbProvider = new db_provider();
+
+            dbProvider.insert_user("email","hash",new []{Permissions.Add, Permissions.Divide, });
+            var result = dbProvider.get_user_info("email");
+            result.user_email.Should().Be("email");
+            result.password_hash.Should().Be("hash");
+            result.permissions.Permissions.Should().ContainInOrder(new[] {Permissions.Add, Permissions.Divide,});
+
+        }
+    }
+}
--- SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core/adapter/db_provider.cs
+++ SecureCalculator/src/CredentionalService/credentionalService.core/credentionalService.core/adapter/db_provider.cs
@@ -1,6 +1,71 @@
-﻿namespace credentionalService.core.adapter
+﻿using System;
+using System.IO;
+using System.Linq;
+using credentionalService.core.data;
+using sc.contracts;
+
+namespace credentionalService.core.adapter
 {
     class db_provider
     {
+        private static string DbFile = "userfile.txt";
+
+        public void insert_user(string email, string passwordHash,Permissions[] initialPermission)
+        {
+            user_info newInfo = new user_info()
+            {
+                password_hash = passwordHash,
+                user_email = email,
+                permissions = new PermissionSet(initialPermission)
+            };
+
+            var userLine = create_line_from_user(newInfo);
+            File.AppendAllLines(DbFile,new[] {userLine});
+        }
+
+
+        public user_info get_user_info(string email)
+        {
+            var users = get_all_users();
+            return users.FirstOrDefault(o => o.user_email.Equals(email));           
+        }
+
+
+        private user_info[] get_all_users()
+        {
+            var lines = File.ReadAllLines(DbFile);
+            return lines.Select(get_user_from_line).ToArray();        
+        }
+
+        private string create_line_from_user(user_info user)
+        {
+            var persimmions = user.permissions.Permissions.Select(o => o.ToString());
+            var permissionsString = persimmions.Aggregate((i, j) => i + "," + j);
+
+            return $"{user.user_email},{user.password_hash},{permissionsString}";
+        }
+
+        private user_info get_user_from_line(string line)
+        {
+            var parts = line.Split(',');
+            var target = new user_info();
+            fill_permissions_from_line(target, parts);
+            fill_usercredentials_from_line(target, parts.Skip(2).ToArray());
+            return target;
+            
+        }
+
+
+        private void fill_permissions_from_line(user_info target, string[] lineParts)
+        {
+            target.user_email = lineParts[0];
+            target.password_hash = lineParts[1];
+        }
+
+        private void fill_usercredentials_from_line(user_info target, string[] lineParts)
+        {            
+            var permissions = lineParts.Select(o => (Permissions)Enum.Parse(typeof(Permissions), o)).ToArray();
+            target.permissions = new PermissionSet(permissions);
+        }
     }
 }
