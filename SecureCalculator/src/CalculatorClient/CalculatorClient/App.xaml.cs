ee1400f7d2b6ae62bf1c9ae7f44758a2b15a48f8
Thomas Mann <thomas.mann@wago.com>
2017-12-19 13:41:22 +0100

    CalculatorClient: added logging

--- SecureCalculator/src/CalculatorClient/CalculatorClient/App.xaml.cs
+++ SecureCalculator/src/CalculatorClient/CalculatorClient/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using log4net;
 
 namespace CalculatorClient
 {
@@ -13,9 +14,20 @@ namespace CalculatorClient
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(App));
+
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            Log.InfoFormat("{0}{0}Starting up...{0}", Environment.NewLine);
+
             Controller.MainController.ShowLogin();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.Info($"Exiting with code {e.ApplicationExitCode}...");
+            base.OnExit(e);
         }
     }
 }
