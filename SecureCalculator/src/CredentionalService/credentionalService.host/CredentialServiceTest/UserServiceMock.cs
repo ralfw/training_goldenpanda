88bd234699d29f8d012b877c0a87792cba799e0f
Siggi <siggiullrich@gmx.de>
2018-01-02 15:49:03 +0100

    service mock with output

--- SecureCalculator/src/CredentionalService/credentionalService.host/CredentialServiceTest/UserServiceMock.cs
+++ SecureCalculator/src/CredentionalService/credentionalService.host/CredentialServiceTest/UserServiceMock.cs
@@ -11,13 +11,13 @@ namespace CredentialServiceTest
 
         public void AddUser(string emailAddress, string rolename, Action onSuccess, Action<string> onError)
         {
-            onError?.Invoke("Error:AddUser");
+            onError?.Invoke($"ServiceMock: AddUser-Command: emailAddress={emailAddress}; rolename={rolename}");
         }
 
         public void LogIn(string emailAddress, string passwordHash, Action<PermissionSet> onSuccess, Action<string> onError)
         {
-            onError?.Invoke("Error:LogIn");
-            onSuccess?.Invoke(new PermissionSet(new Permissions[] {Permissions.Add, Permissions.Divide }));
+            onError?.Invoke($"ServiceMock: LogIn-Command: emailAddress={emailAddress}; passwordHash={passwordHash}");
+            onSuccess?.Invoke(new PermissionSet(new Permissions[] {Permissions.Add, Permissions.Subtract }));
         }
     }
 }
