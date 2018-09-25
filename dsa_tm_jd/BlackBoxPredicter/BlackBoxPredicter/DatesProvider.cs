5ed7f2957272d474391c7b611fed48e22bb6454e
Johann Dück <johann.dueck@wago.com>
2018-09-25 11:15:14 +0200

    Initial Commit For BlackBoxPredicter

---
+++ dsa_tm_jd/BlackBoxPredicter/BlackBoxPredicter/DatesProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoxPredicter
+{
+    public class DatesProvider
+    {
+        public static IList<Tuple<DateTime, DateTime>> GetDates()
+        {
+            return new List<Tuple<DateTime, DateTime>>
+            {
+                new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")),
+                new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")),
+                new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")),
+                new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")),
+                new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")),
+                new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")),
+                new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")),
+                new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")),
+            };
+        }
+    }
+}
---
+++ dsa_tm_jd/BlackBoxPredicter/BlackBoxPredicter_UnitTest/GetDates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BlackBoxPredicter_UnitTest
+{
+    [TestFixture]
+    public class DatesProvider
+    {
+        [Test]
+        public void GetDates()
+        {
+            var result = BlackBoxPredicter.DatesProvider.GetDates();
+
+            result.Count.Should().Be(8);
+            result[0].ShouldBeEquivalentTo(new Tuple<DateTime,DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")));
+        }
+
+        
+    }
+}
