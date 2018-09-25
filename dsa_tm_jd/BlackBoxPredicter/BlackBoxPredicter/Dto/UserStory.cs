5667df6a817da93c24f5480118317db4a4ba4a4a
Thomas Mann <thomas.mann@wago.com>
2018-09-25 13:36:47 +0200

    Created UserStory dto

---
+++ dsa_tm_jd/BlackBoxPredicter/BlackBoxPredicter/Dto/UserStory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BlackBoxPredicter.Dto
+{
+    public class UserStory
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public UserStory(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
