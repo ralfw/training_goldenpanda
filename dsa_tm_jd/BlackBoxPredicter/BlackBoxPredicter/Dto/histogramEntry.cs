b7b981264cab44f33bf7eb7da8311f03b9b35405
Johann Dück <johann.dueck@wago.com>
2018-09-25 15:08:47 +0200

    Added DTO for Histogram

---
+++ dsa_tm_jd/BlackBoxPredicter/BlackBoxPredicter/Dto/histogramEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoxPredicter.Dto
+{
+    public class HistogramEntry
+    {
+        public int CycleTime { get; set; }
+
+        public int Frequence { get; set; }
+
+        public double Percentil { get; set; }
+
+        public HistogramEntry(int cycleTime,int frequence, double percentil)
+        {
+            CycleTime = cycleTime;
+            Frequence = frequence;
+            Percentil = percentil;
+        }
+
+    }
+}
