c64938063a8344a93ca4a5dd4da579a2765971ef
Dimitri Samorukov <dimitri.samorukov@wago.com>
2018-09-25 12:02:35 +0200

    to static

--- dsa_tm_jd/BlackBoxPredicter/BlackBoxPredicter_UnitTest/BackBoxTest.cs
+++ dsa_tm_jd/BlackBoxPredicter/BlackBoxPredicter_UnitTest/BackBoxTest.cs
@@ -25,12 +25,12 @@ namespace BlackBoxPredicter_UnitTest
             dates.Add(new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-02")));
             dates.Add(new Tuple<DateTime, DateTime>(DateTime.Parse("2018-01-01"), DateTime.Parse("2018-01-03")));
 
-            bl.CalculateCycles(dates).Count.Should().Be(2);
-            bl.CalculateCycles(dates)[0]
+            BlackBox.CalculateCycles(dates).Count.Should().Be(2);
+            BlackBox.CalculateCycles(dates)[0]
               .Should()
               .Be(2);
 
-            bl.CalculateCycles(dates)[1]
+            BlackBox.CalculateCycles(dates)[1]
               .Should()
               .Be(3);
 
