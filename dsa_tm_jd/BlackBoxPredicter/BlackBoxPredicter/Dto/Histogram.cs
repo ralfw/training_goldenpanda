73a01c5d6544d72b4cb416ce69d053e58348ac03
Dimitri Samorukov <dimitri.samorukov@wago.com>
2018-09-27 14:20:53 +0200

    Interface  definition

---
+++ dsa_tm_jd/BlackBoxPredicter/BlackBoxPredicter/Dto/Histogram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoxPredicter.Dto
+{
+    class Histogram
+    {
+        public IList<HistogramEntry> Entries { get; set; }
+        public int MarkerIndex { get; set; }
+        public float MarkerValue { get; set; }
+    }
+}
