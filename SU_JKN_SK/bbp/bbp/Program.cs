8ae969a55a682fb3b3156a424b1d79af3e25601c
Sebastian Kruse <sebastian.kruse@wago.com>
2018-10-08 14:16:42 +0200

    Added data provider

--- SU_JKN_SK/bbp/bbp/Program.cs
+++ SU_JKN_SK/bbp/bbp/Program.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using bbp.dto;
-
-namespace bbp
+﻿namespace bbp
 {
     class Program
     {
@@ -10,27 +6,11 @@ namespace bbp
 
         static void Main(string[] args)
         {
-            var testData = GetTestData();
+            var testData = DataProvider.GetUserStories();
 
             var result = Predictor.Predict(testData);
             foreach (var r in result)
                 ConsoleAdapter.Output($"{r.Duration}: {r.AccumulatedPercentile:F3} - {r.Frequency}\n");
-        }
-
-        private static List<UserStory> GetTestData()
-        {
-            var testdata = new List<UserStory>
-            {
-                new UserStory("2018-01-01", "2018-01-02"),
-                new UserStory("2018-01-01", "2018-01-03"),
-                new UserStory("2018-01-01", "2018-01-02"),
-                new UserStory("2018-01-01", "2018-01-07"),
-                new UserStory("2018-01-01", "2018-01-03"),
-                new UserStory("2018-01-01", "2018-01-05"),
-                new UserStory("2018-01-01", "2018-01-03"),
-                new UserStory("2017-12-30", "2018-01-02")
-            };
-            return testdata;
         }
 
         #endregion
