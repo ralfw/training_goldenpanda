9e951c512c94757e49d12a556ee47be45c368a04
Sebastian Kruse <sebastian.kruse@wago.com>
2018-10-11 14:58:46 +0200

    Extend unit tests

--- SU_JKN_SK/bbp/bbp.UnitTest/_Predictor/Predict.cs
+++ SU_JKN_SK/bbp/bbp.UnitTest/_Predictor/Predict.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using bbp.dto;
 using FluentAssertions;
@@ -9,45 +10,117 @@ namespace bbp.UnitTest._Predictor
     [TestFixture]
     public class Predict
     {
-        [Test]
-        public void ShouldPredict()
+        [TestCase(TEST_DATA_KEY_DEFAULT)]
+        [TestCase(TEST_DATA_KEY_ALTERNATIVE)]
+        [TestCase(TEST_DATA_KEY_NO_DATA)]
+        public void ShouldPredict(string testDataKey)
         {
-            const float reliabilityLevel = .83f;
-            var testData = new List<UserStory>
+            var dataArchive = _testDataArchive[testDataKey];
+            var result = (PredictorResultEnumerable) Predictor.Predict(dataArchive.Data, dataArchive.ReliabilityLevel);
+
+            result.ReliabilityLevelIndex.Should().Be(dataArchive.ExpectedReliabilityLevelIndex);
+            result.Count.Should().Be(dataArchive.ExpectedResults.Count());
+
+            var expectedResults = dataArchive.ExpectedResults.ToArray();
+            for (var i = 0; i < result.Count; i++)
             {
-                new UserStory("2018-01-01", "2018-01-02"),
-                new UserStory("2018-01-01", "2018-01-03"),
-                new UserStory("2018-01-01", "2018-01-02"),
-                new UserStory("2018-01-01", "2018-01-07"),
-                new UserStory("2018-01-01", "2018-01-03"),
-                new UserStory("2018-01-01", "2018-01-05"),
-                new UserStory("2018-01-01", "2018-01-03"),
-                new UserStory("2017-12-30", "2018-01-02")
-            };
+                result[i].Duration.Should().Be(expectedResults[i].Duration);
+                result[i].Frequency.Should().Be(expectedResults[i].Frequency);
+                result[i].AccumulatedPercentile.Should().BeApproximately(expectedResults[i].AccumulatedPercentile, .001f);
+            }
+        }
 
-            var result = (PredictorResultEnumerable)Predictor.Predict(testData, reliabilityLevel);
+        // Is this test really necessary??
+        [Test, SetUICulture("en-US")]
+        public void ShouldThrowArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => Predictor.Predict(null, 0));
 
-            result.ReliabilityLevelIndex.Should().Be(2);
+            exception.Should().NotBeNull();
+            exception.GetType().Should().Be(typeof(ArgumentNullException));
+            exception.Message.Should().StartWith("Value cannot be null.");
+        }
 
-            result[0].Duration.Should().Be(2);
-            result[0].Frequency.Should().Be(2);
-            result[0].AccumulatedPercentile.Should().BeApproximately(.25f, .001f);
+        public const string TEST_DATA_KEY_DEFAULT = "DEFAULT";
+        public const string TEST_DATA_KEY_ALTERNATIVE = "ALTERNATIVE";
+        public const string TEST_DATA_KEY_NO_DATA = "NO_DATA";
 
-            result[1].Duration.Should().Be(3);
-            result[1].Frequency.Should().Be(3);
-            result[1].AccumulatedPercentile.Should().BeApproximately(.625f, .001f);
+        private readonly Dictionary<string, TestData> _testDataArchive = new Dictionary<string, TestData>
+        {
+            {
+                TEST_DATA_KEY_DEFAULT,
+                new TestData
+                {
+                    ReliabilityLevel = .83f,
+                    Data = new[]
+                    {
+                        new UserStory("2018-01-01", "2018-01-02"),
+                        new UserStory("2018-01-01", "2018-01-03"),
+                        new UserStory("2018-01-01", "2018-01-02"),
+                        new UserStory("2018-01-01", "2018-01-07"),
+                        new UserStory("2018-01-01", "2018-01-03"),
+                        new UserStory("2018-01-01", "2018-01-05"),
+                        new UserStory("2018-01-01", "2018-01-03"),
+                        new UserStory("2017-12-30", "2018-01-02")
+                    },
+                    ExpectedReliabilityLevelIndex = 2,
+                    ExpectedResults = new[]
+                    {
+                        new PredictorResult(2, .25f, 2),
+                        new PredictorResult(3, .625f, 3),
+                        new PredictorResult(4, .75f, 1),
+                        new PredictorResult(5, .875f, 1),
+                        new PredictorResult(7, 1f, 1)
+                    }
+                }
+            },
 
-            result[2].Duration.Should().Be(4);
-            result[2].Frequency.Should().Be(1);
-            result[2].AccumulatedPercentile.Should().BeApproximately(.75f, .001f);
+            {
+                TEST_DATA_KEY_ALTERNATIVE,
+                new TestData
+                {
+                    ReliabilityLevel = .62f,
+                    Data = new[]
+                    {
+                        new UserStory("2018-01-01", "2018-01-01"),
+                        new UserStory("2018-01-01", "2018-01-01"),
+                        new UserStory("2018-01-01", "2018-01-02"),
+                        new UserStory("2018-01-01", "2018-01-02"),
+                        new UserStory("2018-01-01", "2018-01-02"),
+                        new UserStory("2018-01-01", "2018-01-03"),
+                        new UserStory("2018-01-01", "2018-01-03"),
+                        new UserStory("2017-12-30", "2018-01-03")
+                    },
+                    ExpectedReliabilityLevelIndex = 0,
+                    ExpectedResults = new[]
+                    {
+                        new PredictorResult(1, .25f, 2),
+                        new PredictorResult(2, .625f, 3),
+                        new PredictorResult(3, .875f, 2),
+                        new PredictorResult(5, 1f, 1)
+                    }
+                }
+            },
 
-            result[3].Duration.Should().Be(5);
-            result[3].Frequency.Should().Be(1);
-            result[3].AccumulatedPercentile.Should().BeApproximately(.875f, .001f);
+            {
+                TEST_DATA_KEY_NO_DATA,
+                new TestData
+                {
+                    ReliabilityLevel = 0,
+                    Data = new UserStory[0],
+                    ExpectedReliabilityLevelIndex = 0,
+                    ExpectedResults = new PredictorResult[0]
+                }
+            }
+        };
 
-            result[4].Duration.Should().Be(7);
-            result[4].Frequency.Should().Be(1);
-            result[4].AccumulatedPercentile.Should().BeApproximately(1f, .001f);
+        private class TestData
+        {
+            public float ReliabilityLevel { get; set; }
+            public IEnumerable<UserStory> Data { get; set; }
+
+            public int ExpectedReliabilityLevelIndex { get; set; }
+            public IEnumerable<PredictorResult> ExpectedResults { get; set; }
         }
     }
 }
