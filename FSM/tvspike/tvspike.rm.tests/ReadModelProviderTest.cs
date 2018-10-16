1802d95b97ee71a89c6406e6410b8e314b289ca2
Johann Dück <johann.dueck@wago.com>
2018-10-16 14:43:07 +0200

    Implemented ReadModelProvider

---
+++ FSM/tvspike/tvspike.rm.tests/ReadModelProviderTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using FluentAssertions;
+using NUnit.Framework;
+using tvspike.contracts;
+
+namespace tvspike.rm.tests
+{
+    [TestFixture]
+    public class ReadModelProviderTest
+    {
+        public void Sollte3TermineAus3EventsAufbauen()
+        {
+            var events = Generiere3FakeEvents();
+
+            var result = ReadModelProvider.Aufbauen(events);
+            result.Should().HaveCount(3);
+        }
+
+
+
+        [Test]
+        public void Sollte2TermineAus3NeuUnd1LoeschEventAufbauen()
+        {
+            var events = Generiere3FakeEvents();
+            events.Add(new Event()
+            {
+                Id = events[1].Id,
+                Name = "LoescheTermin",
+                Nummer = events[1].Nummer + 1
+            });
+
+            var result = ReadModelProvider.Aufbauen(events);
+            result.Should().HaveCount(2);
+        }
+
+        [Test]
+        public void Sollte2TermineAus2NeuUnd2GleichenLoeschEventsAufbauen()
+        {
+            var events = Generiere3FakeEvents();
+            events.Add(new Event()
+            {
+                Id = events[1].Id,
+                Name = "LoescheTermin",
+                Nummer = events[1].Nummer + 1
+            });
+            events.Add(new Event()
+            {
+                Id = events[1].Id,
+                Name = "LoescheTermin",
+                Nummer = events[1].Nummer + 2
+            });
+
+            var result = ReadModelProvider.Aufbauen(events);
+            result.Should().HaveCount(2);
+        }
+
+        private static List<Event> Generiere3FakeEvents()
+        {
+            var obj = new TerminRM
+            {
+                Id = Guid.NewGuid().ToString(),
+                Behandler = "Dan",
+                Kunde = "Johann",
+                Von = new DateTime(2018, 10, 16, 18, 00, 00),
+                Bis = new DateTime(2018, 10, 16, 18, 45, 00)
+            };
+            var data = new JavaScriptSerializer().Serialize(obj);
+            var events = new List<Event>();
+            for (int i = 0; i < 3; i++)
+            {
+                events.Add(new Event
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Nummer = DateTime.Now.Ticks,
+                    Daten = data,
+                    Name = "NeuerTermin"
+                });
+            }
+
+            return events;
+        }
+    }
+}
---
+++ FSM/tvspike/tvspike.rm/ReadModelProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using tvspike.contracts;
+
+namespace tvspike.rm
+{
+    public class ReadModelProvider
+    {
+        public static ICollection<TerminRM> Aufbauen(IEnumerable<Event> events)
+        {
+            var sortierteEvents = events.OrderBy(e => e.Nummer);
+            return GeneriereTermine(sortierteEvents);
+        }
+
+        private static List<TerminRM> GeneriereTermine(IOrderedEnumerable<Event> sortierteEvents)
+        {
+            var terminRms = new List<TerminRM>();
+            foreach (var esEvent in sortierteEvents)
+            {
+                switch (esEvent.Name)
+                {
+                    case "NeuerTermin":
+                        var terminRm = ToTerminRm(esEvent);
+                        terminRms.Add(terminRm);
+                        break;
+                    case "LoescheTermin":
+                        terminRms = LoescheTermin(terminRms, esEvent.Id);
+                        break;
+                    default:
+                        throw new ArgumentException("Unbekannter Event Name");
+                }
+            }
+
+            return terminRms;
+        }
+
+        private static List<TerminRM> LoescheTermin(List<TerminRM> aktuelleTermine, string zuLoeschendeTerminId)
+        {
+            var zuLoeschendeTermin = aktuelleTermine.FirstOrDefault(t => t.Id == zuLoeschendeTerminId);
+            if (zuLoeschendeTermin != null)
+                aktuelleTermine.Remove(zuLoeschendeTermin);
+            return aktuelleTermine;
+        }
+
+        private static TerminRM ToTerminRm(Event neuerTerminEvent)
+        {
+            var daten = new JavaScriptSerializer().Deserialize<TerminRM>(neuerTerminEvent.Daten);
+            daten.Id = neuerTerminEvent.Id;
+            return daten;
+        }
+
+
+    }
+
+}
