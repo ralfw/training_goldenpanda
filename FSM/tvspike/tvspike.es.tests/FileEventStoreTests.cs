d2d3dbbfcb09bd2f4e889af819a49b6ce79c2ad4
Thomas Mann <thomas.mann@wago.com>
2018-11-07 15:42:07 +0100

    Started reimplementation of ReplayAll/ReplayFrom

---
+++ FSM/tvspike/tvspike.es.tests/FileEventStoreTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+using tvspike.contracts;
+
+namespace tvspike.es.tests
+{
+    [TestFixture]
+    public class FileEventStoreTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+        }
+
+        [Test]
+        public void ShouldGetAllFileNamesFromTheStore()
+        {
+            // arrange
+            var eventStoreRootFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "events");
+            if (Directory.Exists(eventStoreRootFolder))
+                Directory.Delete(eventStoreRootFolder, true);
+            Directory.CreateDirectory(eventStoreRootFolder);
+
+            const string fileName2 = "test.txt";
+            const string fileName = "test2.txt";
+            File.WriteAllText(Path.Combine(eventStoreRootFolder, fileName), "");
+            File.WriteAllText(Path.Combine(eventStoreRootFolder, fileName2), "");
+            var fileEventStore = new FileEventStore(eventStoreRootFolder);
+
+            // act
+            var allFileNames = fileEventStore.GetAllFileNames();
+
+            // assert
+            allFileNames.Length.Should().Be(2);
+            allFileNames.Should().Contain(x => x.EndsWith(fileName));
+            allFileNames.Should().Contain(x => x.EndsWith(fileName2));
+        }
+    }
+}
---
+++ FSM/tvspike/tvspike.es/FileEventStore.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace tvspike.es
+{
+    public class FileEventStore
+    {
+        private readonly string _absolutePathToWorkingDirectory;
+
+        public string[] GetAllFileNames()
+        {
+            return Directory.GetFiles(_absolutePathToWorkingDirectory);
+        }
+
+        public FileEventStore(string absolutePathToWorkingDirectory)
+        {
+            _absolutePathToWorkingDirectory = absolutePathToWorkingDirectory;
+        }
+    }
+}
