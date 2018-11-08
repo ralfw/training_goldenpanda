01131b526b6007bb24dd37cf77b55cbc92264843
Thomas Mann <thomas.mann@wago.com>
2018-11-08 13:53:47 +0100

    Introduced FileNumberStore responsible for event number handling

---
+++ FSM/tvspike/tvspike.es.tests/FileNumberStoreTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace tvspike.es.tests
+{
+    [TestFixture]
+    public class FileNumberStoreTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+        }
+
+        [Test]
+        public void ShouldGetConsecutiveNumbersWithEachCall()
+        {
+            var storeRootFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "eventStore_0");
+            if(Directory.Exists(storeRootFolder))
+                Directory.Delete(storeRootFolder, true);
+            Directory.CreateDirectory(storeRootFolder);
+            var storageFilePath = Path.Combine(storeRootFolder, "eventnumbers.txt");
+            File.WriteAllText(storageFilePath, 0.ToString());
+
+            var store = new FileNumberStore(storeRootFolder);
+
+            store.NextNumber().Should().Be(1L);
+            store.NextNumber().Should().Be(2L);
+            store.NextNumber().Should().Be(3L);
+
+            File.ReadAllText(storageFilePath).Trim().Should().Be("3");
+        }
+
+        [Test]
+        public void ShouldThrowExceptionOnFullStore()
+        {
+            var storeRootFolder = Path.Combine(TestContext.CurrentContext.TestDirectory, "eventStore_0");
+            if (Directory.Exists(storeRootFolder))
+                Directory.Delete(storeRootFolder, true);
+            Directory.CreateDirectory(storeRootFolder);
+            File.WriteAllText(Path.Combine(storeRootFolder, "eventnumbers.txt"), long.MaxValue.ToString());
+
+            var store = new FileNumberStore(storeRootFolder);
+
+            Action call = () => store.NextNumber();
+
+            call.ShouldThrow<InvalidOperationException>()
+                .WithMessage("Store is full.");
+        }
+    }
+}
---
+++ FSM/tvspike/tvspike.es/FileNumberStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace tvspike.es
+{
+    public class FileNumberStore
+    {
+        private readonly string _storageFilePath;
+
+        public FileNumberStore(string workingFolder)
+        {
+            _storageFilePath = Path.Combine(workingFolder, "eventnumbers.txt");
+        }
+
+        public long NextNumber()
+        {
+            var lastNumber = ReadLastNumber();
+            var nextNumber = GenerateNextNumber();
+            WriteNextNumber();
+            return nextNumber;
+
+            long GenerateNextNumber() => lastNumber + 1;
+            void WriteNextNumber() => File.WriteAllText(_storageFilePath, nextNumber.ToString());
+        }
+
+        private long ReadLastNumber()
+        {
+            var readAllText = File.ReadAllText(_storageFilePath);
+
+            var lastNumber = long.Parse(readAllText.Trim());
+            if (lastNumber == long.MaxValue)
+                throw new InvalidOperationException("Store is full.");
+
+            return lastNumber;
+        }
+    }
+}
