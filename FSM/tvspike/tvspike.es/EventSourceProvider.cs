fef5384b2b79e0814c5d86e6155886a42c4b9281
Thomas Mann <someone@someurl.com>
2018-11-11 14:07:16 +0100

    tvspike.es: Refactored EventSourceProvider

--- FSM/tvspike/tvspike.es/EventSourceProvider.cs
+++ FSM/tvspike/tvspike.es/EventSourceProvider.cs
@@ -21,25 +21,75 @@ namespace tvspike.es
 
     public class EventSourceProvider
     {
-        private readonly string _eventStoreFolderPath;
-        private readonly FileEventStore _fileEventStore;
-        private readonly FileNumberStore _fileNumberStore;
-        private const string FILENAME_CLIENT_ID = "clientId.txt";
-        private const string DIRNAME_EVENTS_SUBDIR = "events";
-
         public string ClientId { get; private set; }
 
         public EventSourceProvider(string eventStoreFolderPath)
         {
-            _eventStoreFolderPath = eventStoreFolderPath;
-
-            InitWorkFolder();
-
-            _fileEventStore = new FileEventStore(Path.Combine(_eventStoreFolderPath, DIRNAME_EVENTS_SUBDIR));
-            _fileNumberStore = new FileNumberStore(_eventStoreFolderPath);
+            StoreEventStoreFolderPath(eventStoreFolderPath);
+            InitializeWorkFolder();
+            // Question: Should we better use a factory to build these? Or a factory which builds this whole instance?
+            BuildDependencies();    
         }
 
-        private void InitWorkFolder()
+        public void Record(Event @event)
+        {
+            @event.Nummer = _fileNumberStore.NextNumber();
+            var eventFilename = EventFilename.From(@event, ClientId).Name;
+            PersistEvent(eventFilename, @event);
+        }
+
+        public void Record(IEnumerable<Event> events)
+        {
+            events.ToList().ForEach(Record);
+        }
+
+        public IEnumerable<Event> ReplayAll()
+        {
+            var eventFileInfos = _fileEventStore.GetAllEventFileInfos();
+            return CreateEvents(eventFileInfos);
+        }
+
+        public IEnumerable<Event> ReplayFor(string eventId)
+        {
+            var eventFileInfos = _fileEventStore.GetEventFileInfosBy(eventId);
+            return CreateEvents(eventFileInfos);
+        }
+
+        private void PersistEvent(string filename, Event @event)
+        {
+            var lines = new[]
+            {
+                $"{filename}",
+                $"{@event.Daten}"
+            };
+            // TODO: use FileEventStore to persist the event /TMa
+            var eventsFolder = Path.Combine(_eventStoreFolderPath, DIRNAME_EVENTS_SUBDIR);
+            File.WriteAllLines(Path.Combine(eventsFolder, filename), lines);
+        }
+
+        private IEnumerable<Event> CreateEvents(EventFileInfo[] eventFileInfos)
+        {
+            return eventFileInfos.Select(CreateEvent);
+        }
+
+        private Event CreateEvent(EventFileInfo eventFileInfo)
+        {
+            var parsedNumber = long.Parse(eventFileInfo.EventNumber);
+            return new Event
+            {
+                Nummer = parsedNumber,
+                Id = eventFileInfo.EventId,
+                Name = eventFileInfo.EventName,
+                Daten = eventFileInfo.EventData
+            };
+        }
+
+        private void StoreEventStoreFolderPath(string eventStoreFolderPath)
+        {
+            _eventStoreFolderPath = eventStoreFolderPath;
+        }
+
+        private void InitializeWorkFolder()
         {
             EnsureWorkingDirectoryStructure(_eventStoreFolderPath);
             ClientId = GetClientId(_eventStoreFolderPath);
@@ -69,57 +119,17 @@ namespace tvspike.es
             return clientId;
         }
 
-        public void Record(Event @event)
+        private void BuildDependencies()
         {
-            @event.Nummer = _fileNumberStore.NextNumber();
-            var eventFilename = EventFilename.From(@event, ClientId).Name;
-            PersistEvent(eventFilename, @event);
+            _fileEventStore = new FileEventStore(Path.Combine(_eventStoreFolderPath, DIRNAME_EVENTS_SUBDIR));
+            _fileNumberStore = new FileNumberStore(_eventStoreFolderPath);
         }
 
-        public void Record(IEnumerable<Event> events)
-        {
-            events.ToList().ForEach(Record);
-        }
+        private const string FILENAME_CLIENT_ID = "clientId.txt";
+        private const string DIRNAME_EVENTS_SUBDIR = "events";
 
-        private void PersistEvent(string filename, Event @event)
-        {
-            var lines = new[]
-            {
-                $"{filename}",
-                $"{@event.Daten}"
-            };
-            // TODO: use FileEventStore to persist the event /TMa
-            var eventsFolder = Path.Combine(_eventStoreFolderPath, DIRNAME_EVENTS_SUBDIR);
-            File.WriteAllLines(Path.Combine(eventsFolder, filename), lines);
-        }
-
-        public IEnumerable<Event> ReplayAll()
-        {
-            var eventFileInfos = _fileEventStore.GetAllEventFileInfos();
-            return CreateEvents(eventFileInfos);
-        }
-
-        public IEnumerable<Event> ReplayFor(string eventId)
-        {
-            var eventFileInfos = _fileEventStore.GetEventFileInfosBy(eventId);
-            return CreateEvents(eventFileInfos);
-        }
-
-        private IEnumerable<Event> CreateEvents(EventFileInfo[] eventFileInfos)
-        {
-            return eventFileInfos.Select(CreateEvent);
-        }
-
-        private Event CreateEvent(EventFileInfo eventFileInfo)
-        {
-            var parsedNumber = long.Parse(eventFileInfo.EventNumber);
-            return new Event
-            {
-                Nummer = parsedNumber,
-                Id = eventFileInfo.EventId,
-                Name = eventFileInfo.EventName,
-                Daten = eventFileInfo.EventData
-            };
-        }
+        private string _eventStoreFolderPath;
+        private FileEventStore _fileEventStore;
+        private FileNumberStore _fileNumberStore;
     }
 }
