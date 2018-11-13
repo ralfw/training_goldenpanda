6024cf3b297b1864dfdde90d27ac9720c027f368
Thomas Mann <thomas.mann@wago.com>
2018-11-13 13:58:26 +0100

    AppointmentExporter: Added project for SqLite access

---
+++ AppointmentExporter/SqLiteProviderLib/SqLiteReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppointmentContract;
+
+namespace SqLiteProviderLib
+{
+    public class SqLiteReader
+    {
+        public IEnumerable<Appointment> ReadDataFromSqLite(DateTime begin, DateTime end)
+        {
+            return null;
+        }
+    }
+}
