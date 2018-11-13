4b77e589102edb0b60296b1e6c6099de85304e52
Siggi <siggi.ullrich@wago.com>
2018-11-13 14:33:22 +0100

    moved files to folders

--- AppointmentExporter/AppointmentExporter/View/AppointmentExporterWindow.xaml.cs
+++ AppointmentExporter/AppointmentExporter/View/AppointmentExporterWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using AppointmentExporter.ViewModel;
 
-namespace AppointmentExporter
+namespace AppointmentExporter.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,8 +13,10 @@ namespace AppointmentExporter
             InitializeComponent();
             var vm = new AppointmentExporterVM();
             var  fakeRequestHandler = new FakeRequestHandler();
+
             vm.onExport += fakeRequestHandler.Export;
             fakeRequestHandler.onExported += vm.DisplayStatus;
+
             DataContext = vm;
         }
     }
