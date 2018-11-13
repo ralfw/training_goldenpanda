1a86ef7f4317ee3e5545e266d9105f59cbbd7ef7
Johann Dück <johann.dueck@wago.com>
2018-11-13 13:48:23 +0100

    Implemented AppointmentContract

---
+++ AppointmentExporter/AppointmentContract/Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppointmentContract
+{
+    public class Appointment
+    {
+        public DateTime Begin { get; set; }
+        public DateTime End { get; set; }
+
+        public string Subject { get; set; }
+    }
+}
